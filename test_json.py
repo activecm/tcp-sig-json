02013d6dc3fcd3fef1ab416f421d48fff857df27
David Quartarolo <david.quartarolo@gmail.com>
2022-08-01 21:17:31 -0400

    Updated signatures

--- test_json.py
+++ test_json.py
@@ -7,11 +7,9 @@ import json
 import unittest
 
 
-
-
 class TestTcpJsonFile(unittest.TestCase):
 
-    def validateDict(self):
+    def test_validateDict(self):
         '''Validate JSON data is accessible.'''
         try:
             jsonFile = open('tcp-sig.json')
@@ -20,12 +18,12 @@ class TestTcpJsonFile(unittest.TestCase):
         except ValueError as err:
             return False
 
-    def validateKeys(self):
+    def test_validateKeys(self):
         '''Validate JSON data is accessible.'''
         try:
             jsonFile = open('tcp-sig.json')
             tcp_sig = json.load(jsonFile)
-            json_keys = list(['signature_list'][0].keys())
+            json_keys = list(tcp_sig['signature_list'][0].keys())
             self.assertEqual(json_keys, ['acid', 'platform', 'tcp_flag', 'tcp_sig', 'comments'])
         except ValueError as err:
             return False
