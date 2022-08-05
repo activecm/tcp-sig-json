ab6e9ed4bc273efa068205202058c9af9f1ea92c
David Quartarolo <david.quartarolo@sectigo.com>
2022-08-05 09:55:51 -0400

    test: validate unique ACID keys

--- test_json.py
+++ test_json.py
@@ -22,6 +22,14 @@ class TestTcpJsonFile(unittest.TestCase):
         json_keys = list(tcp_sig['signature_list'][0].keys())
         self.assertEqual(sorted(json_keys), sorted(['acid', 'platform', 'tcp_flag', 'tcp_sig', 'comments']))
 
+    def test_validateACID(self):
+        '''Validate every ACID key is unique.'''
+        jsonFile = open('tcp-sig.json')
+        tcp_sig = json.load(jsonFile)
+        acid_list = [sig['acid'] for sig in tcp_sig['signature_list']]
+        self.assertTrue(len(acid_list) == len(set(acid_list)))
+
+
 
 
 if __name__ == "__main__":
